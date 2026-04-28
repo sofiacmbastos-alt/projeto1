d974c0fdbed970b8f79fb49f75e0e1a0b5dd7db6
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-04-28 15:41:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,8 +7,8 @@ st.image("https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=20250108142
 
 st.video("https://www.youtube.com/watch?v=uUTraxYVf8o")
 
-nome = st.text_input('Digite o seu nome')
+nome = st.text_input('type your name here')
 if nome:
-    st.write(nome, 'é um cara legal')
+    st.write(nome, 'is super cuteee *-*')
 
 st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Netherlandwarf.jpg/330px-Netherlandwarf.jpg")
