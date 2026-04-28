d616bb9332ea495cdd515d2bd7a5c41e7618b4db
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-04-28 15:21:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,5 @@ st.title('Teste ECMI 2')
 st.write("Tabela")
 
 st.image("https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=202501081421", caption="Remote Image")
+
 st.image("https://github.com/sofiacmbastos-alt/projeto1/blob/main/IMG_4942.jpeg")
