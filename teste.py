a72dd5472d6bad176af018d3b5ee305d7a3fe4c9
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-04-28 15:17:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title('Teste ECMI 2')
 st.write("Tabela")
 
-st.image("https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fmy-friend-keeps-sending-these-hamsters-as-reaction-images-v0-pxf1726z385f1.jpg%3Fwidth%3D1170%26format%3Dpjpg%26auto%3Dwebp%26s%3D2d867eddd088b3ee582f88091d60ae145c3639c0", caption="Remote Image")
+st.image("https://github.com/sofiacmbastos-alt/projeto1/blob/main/IMG_4942.jpeg", caption="Remote Image")
