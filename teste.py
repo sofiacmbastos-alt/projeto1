43f4709d2ad71695059b18cf8acb2ddf1238c44a
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-04-28 15:25:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,3 +6,5 @@ st.write("Tabela")
 st.image("https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=202501081421", caption="Remote Image")
 
 st.video("https://www.youtube.com/watch?v=uUTraxYVf8o")
+
+st.image('https://github.com/sofiacmbastos-alt/projeto1/blob/main/IMG_4942.jpeg')
