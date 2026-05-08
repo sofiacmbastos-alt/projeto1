a2ace1e09fa979b0a41980e5f0996c01276b45bc
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-05-08 10:39:23 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,27 @@
 import streamlit as st
 
-st.title('Teste ECMI 2')
+st.markdown(
+    "<h1 style='color:#FFD1DC;'>Teste ECMI 2 🌸</h1>",
+    unsafe_allow_html=True
+)
+
 st.write("Tabela")
 
-st.image("https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=202501081421", caption="Remote Image")
+st.image(
+    "https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=202501081421",
+    caption="Remote Image"
+)
 
 st.video("https://www.youtube.com/watch?v=uUTraxYVf8o")
 
 nome = st.text_input('type your name here')
-if nome:
-    st.write(nome, 'is super cuteee')
 
-st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Netherlandwarf.jpg/330px-Netherlandwarf.jpg")
+if nome:
+    st.markdown(
+        f"<p style='color:#FFD1DC; font-size:20px;'>{nome} is super cuteee 🩷</p>",
+        unsafe_allow_html=True
+    )
+
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Netherlandwarf.jpg/330px-Netherlandwarf.jpg"
+)
