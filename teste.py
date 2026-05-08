664c52664f5f74a01aab52f687dccc658b7f8297
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-05-08 10:44:14 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,16 +11,6 @@ st.image(
     "https://polipet.fbitsstatic.net/media/hollandpopicon.png?v=202501081421"
 )
 
-st.video("https://www.youtube.com/watch?v=uUTraxYVf8o")
-
-nome = st.text_input('type your name here')
-
-if nome:
-    st.markdown(
-        f"<p style='color:#FFD1DC; font-size:20px;'>{nome} is super cuteee 🩷</p>",
-        unsafe_allow_html=True
-    )
-
 st.image(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Netherlandwarf.jpg/330px-Netherlandwarf.jpg"
 )
